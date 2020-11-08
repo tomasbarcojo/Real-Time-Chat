06aae5b275efc52717b4a613ffeb1e5b7b231460
Tomas <tomasbarcojo@gmail.com>
2020-11-08 12:53:33 -0300

    New backend and frontend files

--- api/index.js
+++ api/index.js
@@ -1,29 +1,15 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { servidor } = require('./src/socket')
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
+const PORT = process.env.PORT || 4000
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => (
+    console.log(`Server has started on port ${PORT}`)
+))
+
+
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Join from './Components/Join'
+import Chat from './Components/Chat'
+
+const App = () => (
+    <Router>
+        <Route exact path="/" component={Join} />
+        <Route exact path="/chat" component={Chat} />
+    </Router>
+)
+
+export default App
