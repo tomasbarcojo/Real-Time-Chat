ae01bf7300ae7a8867c95e81895610a17a00a381
Tomas <tomasbarcojo@gmail.com>
2020-11-08 13:03:36 -0300

    Socket running and default route for backend

--- api/index.js
+++ api/index.js
@@ -4,9 +4,21 @@ const http = require('http');
 
 const PORT = process.env.PORT || 4000
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('New connection')
+
+    socket.on('disconnect', () => {
+        console.log('User has left')
+    })
+})
+
+app.use(router);
 
 server.listen(PORT, () => (
     console.log(`Server has started on port ${PORT}`)
