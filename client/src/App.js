3c1bcefe804eb840d6ef2f454bb4230cb60e8514
Tomas <tomasbarcojo@gmail.com>
2020-11-08 13:29:59 -0300

    App structuring and main page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Join from './Components/Join'
-import Chat from './Components/Chat'
+import Join from './Components/Join/Join'
+import Chat from './Components/Chat/Chat'
 
 const App = () => (
     <Router>
